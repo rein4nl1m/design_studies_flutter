32957916b155c593c257610c16d333bd3febe769
Reinan Pereira Lima <reinan.lima@hbsis.com.br>
2019-10-04 14:27:27 -0300

    final class widgts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:design_app/button_text_icon.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,47 +11,40 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          color: Colors.yellow,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/background.jpg"),
+              fit: BoxFit.cover
+            ),
+          ),
+          //color: Colors.yellow,
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ButtonIcon(),
-              ButtonText()
+              Image.asset("assets/logo.png", scale: 2),
+              ButtonTextIcon(
+                color: Colors.red,
+                text: "Login com Google",
+                iconData: Icons.add,
+              ),
+              ButtonTextIcon(
+                color: Colors.blue,
+                text: "Login com Facebook",
+                iconData: Icons.wifi,
+              ),
+              ButtonTextIcon(
+                color: Colors.black,
+                text: "Login com Email",
+                iconData: Icons.mail,
+              )
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class ButtonIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 50,
-      color: Colors.red,
-      child: Icon(Icons.add, color: Colors.white),
-    );
-  }
-}
-
-class ButtonText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: double.infinity,
-        height: 50,
-        color: Colors.blue,
-        alignment: Alignment.center,
-        child: Text("Toque aqui", style: TextStyle(color: Colors.white, fontSize: 18),),
       ),
     );
   }
